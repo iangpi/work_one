13de3282df12b13c2db3b8eed927ad042d05cffc
iangpi <0413iangpi@gmail.com>
2016-04-05 18:43:02 +0800

    update at company

--- base/PPMPFind_el.py
+++ base/PPMPFind_el.py
@@ -1,33 +1,36 @@
 #coding:utf-8
 from PPMPChoseBrowser import The_Driver
 import time
+browser_name='ff'
+the_url=r"http://www.baidu.com"
+my_driver=The_Driver()#实例化类
 #封装定位方法
-my_driver=The_Driver()#实例化类，浏览器的选择
 class Find_Element(object):
-    def by_id(self,the_id,browser_name,the_url):
+    def by_id(self,the_id):
         dr=my_driver.the_browser(browser_name)
         dr.get(the_url)
         return dr.find_element_by_id(the_id)
 
-    def by_xpath(self,the_xpath,browser_name,the_url):
+    def by_xpath(self,the_xpath):
         dr=my_driver.the_browser(browser_name)
         dr.get(the_url)
         return dr.find_element_by_xpath(the_xpath)
 
-    def by_classname(self,the_classname,browser_name,the_url):
+    def by_classname(self,the_classname):
         dr=my_driver.the_browser(browser_name)
         dr.get(the_url)
         return dr.find_element_by_class_name(the_classname)
+
 if __name__=="__main__":
     mytest=Find_Element()
-    mytest.by_id('kw','ff',r'http://www.baidu.com').send_keys('12306')
+    mytest.by_id('kw').send_keys('12306')
     time.sleep(3)
     my_driver.close_browser()
 
-    mytest.by_xpath(".//*[@id='kw']",'ff',r'http://www.baidu.com').send_keys('12306')
+    mytest.by_xpath(".//*[@id='kw']").send_keys('12306')
     time.sleep(3)
     my_driver.close_browser()
 
-    mytest.by_classname("cp-feedback",'ff',r'http://www.baidu.com').click()
+    mytest.by_classname("cp-feedback").click()
     time.sleep(5)
     my_driver.close_browser()
--- base/PPMPLogin.py
+++ base/PPMPLogin.py
@@ -1,22 +1,26 @@
 #coding:utf-8
 from selenium import webdriver
-from PPMPChoseBrowser import Chose_Browser
+from PPMPChoseBrowser import The_Driver
+from PPMPFind_el import Find_Element
 import sys,os
-#避免出现乱码
+
 reload(sys)
-sys.setdefaultencoding('utf-8')
-#跨文件引用
-parentdir=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.setdefaultencoding('utf-8')#避免出现乱码
+parentdir=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))#跨文件引用
 sys.path.insert(0,parentdir)
+
 el_username='username'
 el_password='password'
 el_submit='submit'
 
+my_driver=The_Driver()#实例化类
+my_el=Find_Element()
+
 class Login(object):
-    def LoginSuccess(self,username,password):
-        the_browser=Chose_Browser()#实例化打开浏览器类
-        the_browser.which_Browser('ff')
-        the_browser.open_Browser(r'http://awsbj-openmanagement.xingyunzhi.cn/developer-user/login')
+    def LoginSuccess(self,username,password,browser_name,the_url):
+        my_driver.open_browser(browser_name)
+
+
 if __name__=="__main__":
     Login().LoginSuccess('wxg','wxg')
 
