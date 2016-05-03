5fa3805a55c94d21c9ad9c6136767fb13809375f
iangpi <0413iangpi@gmail.com>
2016-05-03 19:14:06 +0800

    update at company

--- pp_management_platform/v_4/base/MyUnit.py
+++ pp_management_platform/v_4/base/MyUnit.py
@@ -9,17 +9,15 @@ from MyBase import Base
 
 class My_setup_teardown(unittest.TestCase):
     def setUp(self):
-        print ('test start')
-        self.driver=browsers['ff']
-        self.the_dr=Base(self.driver)
-        #实例化MyBase里的Login类，并登录
-        login=Login(self.driver)
+        print ("test start")
+        #实例化MyBase里的Base类
+        driver=browsers['ff']
+        self.the_dr=Base(driver)
+        #实例化LoginPage里的Login类，并登录
+        login=Login(driver)
         login.loginpage()
-        self.driver.implicitly_wait(15)
     def tearDown(self):
         self.the_dr.quit_browser()
-        print ('test end')
-    def test_001(self):
-        pass
+        print ("test end")
 if __name__=="___main_":
     unittest.main()
