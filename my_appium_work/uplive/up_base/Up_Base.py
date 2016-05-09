39cc916ca7f07ab3e8bbb5b142653478b910a5e6
iangpi <0413iangpi@gmail.com>
2016-05-09 19:08:54 +0800

    study starta_activity fun

---
+++ my_appium_work/uplive/up_base/Up_Base.py
@@ -0,0 +1,26 @@
+#coding:utf-8
+from appium import webdriver
+import os
+import time
+import sys
+sys.path.append('./base')
+PATH=lambda p:os.path.abspath(os.path.join(os.path.dirname(__file__),p))
+
+desired_caps={}
+desired_caps['device'] = 'android'
+desired_caps['platformName']='Android'#系统
+desired_caps['browserName']=''
+# desired_caps['version']='6.0.1'#nexus5的系统版本
+# desired_caps['deviceName']='06b947cf00691c51'#这是nexus5的型号，adb devices获得
+desired_caps['version']='5.0.2'#红迷note2的系统版本
+desired_caps['deviceName']='6LJZW4DUDQCM8L5T'#红迷note2的型号
+# 被测试的App在电脑上的位置，如果知道被测试对象的apppage，appActivity可以加上下面这两个参数，如果不知道，可以注释掉，不影响用例执行
+desired_caps['appPackage']='com.asiainno.uplive'#uplive的
+desired_caps['appActivity']='com.asiainno.uplive.init.splash.SplashActivity'#uplive的
+# desired_caps['appPackage']='com.tencent.mm'#weixin
+# desired_caps['appActivity']='com.tencent.mm.ui.LauncherUI'#weixin
+
+
+dr=webdriver.Remote('http://localhost:4723/wd/hub',desired_caps)
+time.sleep(3)
+dr.start_activity('com.asiainno.daidai','com.asiainno.daidai.init.ui.SplashActivity')
--- my_appium_work/uplive/uptest/uptest001.py
+++ my_appium_work/uplive/uptest/uptest001.py
@@ -1,5 +1,5 @@
 #coding:utf-8
-#给手手机快速装app的方法
+#快速获取uid
 from appium import webdriver
 import os
 import sys
@@ -11,21 +11,9 @@ desired_caps['platformName']='Android'#系统
 desired_caps['browserName']=''
 desired_caps['version']='6.0.1'
 desired_caps['deviceName']='0a609382'
-desired_caps['app'] = PATH(r'E:\daidai_2016.04.15_1.0.0_1040.production_yzl.apk')
+# desired_caps['version']='5.0.2'#红迷note2的系统版本
+# desired_caps['deviceName']='6LJZW4DUDQCM8L5T'#红迷note2的型号
+desired_caps['app'] = PATH('D:\test_app\up_2016.05.08_1.0.0_667.production_localDevelop.apk')
 
 #联系从屏幕获取坐标后，模拟滑滑动操作
 dr=webdriver.Remote('http://localhost:4723/wd/hub',desired_caps)
-'''python中is和==的区别'''
-a=1
-b=1.0
-print id(a)
-print id(b)
-print a==b
-print a is b
-
-c=2
-d=2
-print id(c)
-print id(d)
-print c==d
-print c is d
