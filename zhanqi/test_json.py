d9d8959039658e9381411f13d4535c6ef91d7b87
iangpi <0413iangpi@gmail.com>
2016-05-20 18:38:10 +0800

    do a work for skills,monster and unlockskill

--- zhanqi/test_json.py
+++ zhanqi/test_json.py
@@ -1,4 +1,5 @@
 #coding:utf-8
+'''
 import json
 s = json.loads('{"name":"test", "type":{"name":"seq", "parameter":["1", "2"]}}')
 print s
@@ -6,3 +7,27 @@ print s.keys()
 print s["name"]
 print s["type"]["name"]
 print s["type"]["parameter"][1]
+'''
+
+'''
+a=int(9)
+print '%d'%a
+'''
+
+'''
+
+for a in xrange(0,10,3):
+    b=a+1
+    print b
+for a in xrange(11,20,2):
+    b=a+1
+    print b
+'''
+
+def a():
+    for n in xrange(10):
+        if n==9:
+            a=n
+    return a
+if __name__=="__main__":
+    a()
