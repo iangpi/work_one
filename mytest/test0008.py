eef8d6e60d7256d5d1a0a74b6e548a95a84a7503
iangpi <0413iangpi@gmail.com>
2016-06-07 19:14:51 +0800

    finish 70% at company

--- mytest/test0008.py
+++ mytest/test0008.py
@@ -1,3 +1,4 @@
+'''
 l1=['a','b','c','d']
 l2=['a','d']
 res=[]
@@ -5,3 +6,13 @@ for i in l1:
     if i in l2:
         res.append(i)
 print res
+'''
+'''
+for i in xrange(5):
+    print i
+print 11111111111111111111
+for i in xrange(5,10):
+    print i
+'''
+a=[1,[2,3]]
+print len(a)
--- zhanqi/readskills_v5..py
+++ zhanqi/readskills_v5..py
@@ -49,10 +49,10 @@ class hero_card(object):
         return cards_id
 
     def all_cards_skill(self):
-        cards_skill=[]
+        cards_skill=[]#每三个id一样的放到一个小list里，最后放到这个cards_skill里
         for n in xrange(self.unlock_skill_num):
+            #在unlock表里，如果一张卡的id和第三张的id相等，那么1，2，3张分别是主动，被动，天赋
             card_skill=[]
-            #在unlock表里，如果一张卡的id第三张的相等，那么1，2，3张分别是主动，被动，天赋
             if self.unlock_skill_num-n>3 and self.read_unlock_skill[n]['id']==self.read_unlock_skill[n+2]['id']:
                 card_skill.append(self.read_unlock_skill[n])
                 card_skill.append(self.read_unlock_skill[n+1])
@@ -79,44 +79,72 @@ class hero_card(object):
         mycalss=hero_card()
         fun1=mycalss.all_cards_id()
         fun2=mycalss.all_cards()
+        fun3=mycalss.all_cards_skill()
+        #参数化数据存放
+        row_1_part_1=['编号','名字','输出类型','子类型','卡牌类型','解锁等级','可收集否','可移动否','形象','描述','品质','尺寸','速度']
+        row_2_part_1=['id','name','type','subType','unitType','unlockLevel','collectable','moveable','avatarId','desc','quality','size','speed']
+        row_1_part_2=['编号','主动技能',
+                      '1级解锁技能','技能等级',
+                      '4级解锁技能','技能等级',
+                      '7级解锁技能','技能等级',
+                      '10级解锁技能','技能等级',
+                      '12级解锁技能','技能等级',
+                      '14级解锁技能','技能等级',
+                      '16级解锁技能','技能等级',
+                      '18级解锁技能','技能等级',
+                      '20级解锁技能','技能等级'
+                      ]
+        row_2_part_2=['id','type',
+                      'skillId1','skillLevel1',
+                      'skillId4','skillLevel4',
+                      'skillId7','skillLevel7',
+                      'skillId10','skillLevel10',
+                      'skillId12','skillLevel12',
+                      'skillId14','skillLevel14',
+                      'skillId16','skillLevel16',
+                      'skillId18','skillLevel18',
+                      'skillId20','skillLevel20'
+                      ]
         #新建并打开一个表格(解决思路：先新建表格，再写入数据，数据从哪里来，根据id的唯一性，查询而来）
         book=xlwt.Workbook(encoding='utf-8',style_compression=0)
         #卡牌表有多长，就新建多少个sheet分页，并且用卡牌表中的名字命名分页
         for n in xrange(len(fun1)):#直接实例化自己类里的方法直接用
             sheet_name=str(fun1[n])
             sheet=book.add_sheet(sheet_name,cell_overwrite_ok=True)
-            row_1=['编号','名字','输出类型','子类型','卡牌类型','解锁等级','可收集否','可移动否','形象','描述','品质','尺寸','速度']
-            row_2=['id','name','type','subType','unitType','unlockLevel','collectable','moveable','avatarId','desc','quality','size','speed']
-            num=len(row_1)
-            for m in xrange(num):
-                sheet.write(m,0,row_1[m])
-                sheet.write(m,1,str(fun2[n][row_2[m]]))
-        #保存表格
+            sheet.write(0,0,'基本信息')
+            sheet.write(1,1,'')
+            num_1=len(row_1_part_1)
+            for m in xrange(num_1):
+                sheet.write(m+1,0,row_1_part_1[m])
+                sheet.write(m+1,1,str(fun2[n][row_2_part_1[m]]))
+
+            #下面的+2+3是为了横向写三列，分别是主动，被动，天赋
+            sheet.write(num_1+2,0,'主动技能')
+            sheet.write(num_1+2,1,'')
+            sheet.write(num_1+2,2,'被动技能')
+            sheet.write(num_1+2,3,'')
+            sheet.write(num_1+2,4,'天赋技能')
+            sheet.write(num_1+2,5,'')
+            #获取遍历的次数
+            num_2=len(row_1_part_2)
+            #遍历主动技能，写入
+            for w in xrange(num_2):
+                sheet.write(w+num_1+3,0,row_1_part_2[w])
+                sheet.write(w+num_1+3,1,str(fun3[n][0][row_2_part_2[w]]))
+            #遍历被动技能，写入
+            for x in xrange(num_2):
+                sheet.write(x+num_1+3,2,row_1_part_2[x])
+                sheet.write(x+num_1+3,3,str(fun3[n][1][row_2_part_2[x]]))
+            #遍历天赋技能，写入
+            for y in xrange(num_2):
+                sheet.write(y+num_1+3,4,row_1_part_2[y])
+                sheet.write(y+num_1+3,5,str(fun3[n][2][row_2_part_2[y]]))
+        #写入过后，保存表格
         book.save(r'D:\mygit\work_one\zhanqi\report\user_cards.xls')
-
-
 
 if __name__=="__main__":
     test01=hero_card()
-    # for n in xrange(len(test01.all_cards())):
-    #     print '玩家卡牌：',test01.all_cards()[n]['id']
-    # for n in xrange(len(test01.all_cards())):
-    #     print 'npc卡牌：',test01.all_cards()[n]['name']
-    #test01.write_info_to_excel()
-    print test01.all_cards_skill()
+    test01.write_info_to_excel()
+    #确保技能表除以3的结果恒等于怪物表的里怪物的数量
     print len(test01.all_cards_skill())
     print test01.monster_num
-    res1=[]
-    res2=[]
-    for n in xrange(test01.monster_num):
-        res1.append(test01.read_monster[n]['id'])
-        print '第'+str(n)+'个'+test01.read_monster[n]['id']
-    print '=========================================='
-    for n in xrange(len(test01.all_cards_skill())):
-        res2.append(test01.all_cards_skill()[n][0]['id'])
-        print '第'+str(n)+'个'+test01.all_cards_skill()[n][0]['id']
-    deff=[]
-    for i in res1:
-        if i not in res2:
-            deff.append(i)
-    print deff
